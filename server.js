1b42c977d050db3f275659f79eda501d493a9ddb
amjad-2 <144667008+member-inteam@users.noreply.github.com>
2025-06-30 17:12:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = require('./app');
 const { default: mongoose } = require('mongoose');
 
 const DB = "mongodb://amjadtadmory:<db_password>@ac-eu1ptum-shard-00-00.u9cao07.mongodb.net:27017,ac-eu1ptum-shard-00-01.u9cao07.mongodb.net:27017,ac-eu1ptum-shard-00-02.u9cao07.mongodb.net:27017/?ssl=true&replicaSet=atlas-10srv4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
-    .replace('<db_password>', 'YImFBhDjluvbjI8E')
+    .replace('<db_password>', 'CVyTFczH1nNOnyZ9')
 mongoose.connect(DB, {
     // useNewUrlParser: true,
     // useCreateIndex: true,

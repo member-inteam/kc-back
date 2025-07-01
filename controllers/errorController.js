ee584fdb5a34839aeb72f46724a8eb43506994d3
penko <penkoproko93@gmail.com>
2025-07-01 12:16:23 +0300

    .

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -58,6 +58,6 @@ module.exports = (err, req, res, next) => {
         if (err.name === 'ValidationError') error = handleValidationErrorDB(error)
         if (err.name === 'JsonWebTokenError') error = handleJWTError()
         if (err.name === 'TokenExpiredError') error = handleTokenExpiredError()
-        sendErrorProd(error, res)
+        sendErrorDev(error, res)
     }
 }
